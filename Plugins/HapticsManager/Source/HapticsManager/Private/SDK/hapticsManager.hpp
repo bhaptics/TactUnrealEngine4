1bbe9da10b644e9ec3d74967265b90d0fdc6817c
Stuart Burmeister <stuart@bhaptics.com>
2017-11-03 17:47:43 +0900

    Feedback files

--- Plugins/HapticsManager/Source/HapticsManager/Private/SDK/hapticsManager.hpp
+++ Plugins/HapticsManager/Source/HapticsManager/Private/SDK/hapticsManager.hpp
@@ -176,9 +176,6 @@ namespace bhaptics
         void doRepeat()
         {
             reconnect();
-			mtx.lock();
-			checkMessage();
-			mtx.unlock();
 
             _currentTime += _interval;
         }
