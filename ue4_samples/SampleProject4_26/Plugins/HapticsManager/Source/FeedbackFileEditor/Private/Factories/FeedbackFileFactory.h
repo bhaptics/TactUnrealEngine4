693257f601edd2280a64ea034aeef34ea796234c
Sangwon Choi <sangwon.choi@bhaptics.com>
2021-02-26 16:26:32 +0900

    Update 4.26

--- ue4_samples/SampleProject4_26/Plugins/HapticsManager/Source/FeedbackFileEditor/Private/Factories/FeedbackFileFactory.h
+++ ue4_samples/SampleProject4_26/Plugins/HapticsManager/Source/FeedbackFileEditor/Private/Factories/FeedbackFileFactory.h
@@ -18,5 +18,4 @@ class UFeedbackFileFactory : public UFactory
 
 public :
 	virtual UObject* FactoryCreateFile(UClass* InClass, UObject* InParent, FName InName, EObjectFlags Flags, const FString& Filename, const TCHAR* Parms, FFeedbackContext* Warn, bool& bOutOperationCanceled) override;
-	
 };
