0c850888ca6ea2b57dcf6423920baa74e769f6cb
Sangwon Choi <sangwon.choi@bhaptics.com>
2021-06-07 11:51:54 +0900

    Fix whitespace

--- ue4_samples/SampleProject4_22/Plugins/HapticsManager/Source/FeedbackFileEditor/FeedbackFileEditor.Build.cs
+++ ue4_samples/SampleProject4_22/Plugins/HapticsManager/Source/FeedbackFileEditor/FeedbackFileEditor.Build.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+﻿// Copyright 2015~2021 bHaptics Inc. All Rights Reserved.
 
 
 using System;
@@ -6,37 +6,34 @@ using System.IO;
 using UnrealBuildTool;
 
 public class FeedbackFileEditor : ModuleRules
-{
-    public FeedbackFileEditor(ReadOnlyTargetRules Target) : base (Target)
-	{
+{ 
+    public FeedbackFileEditor(ReadOnlyTargetRules Target) : base (Target) 
+    {
         PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
         PrivatePCHHeaderFile = "Private/FeedbackFileEditor.h";
 
         PrivateIncludePaths.AddRange(
-			new string[] {
+            new string[] {
                 "FeedbackFileEditor/Private",
-				// ... add other private include paths required here ...
-			}
-			);
-			
-		PublicDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"Core",
+            }
+            );
+        
+        PublicDependencyModuleNames.AddRange(
+            new string[] 
+            {
+                "Core",
                 "UnrealEd"
-                //"System",
-				// ... add other public dependencies that you statically link with here ...
-			}
-			);
-			
-		
-		PrivateDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"CoreUObject",
-				"Engine",
-				"Slate",
-				"SlateCore",
+
+            }
+            );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[] 
+            {
+                "CoreUObject", 
+                "Engine", 
+                "Slate", 
+                "SlateCore", 
                 "FeedbackFileEditor",
                 "Json",
                 "JsonUtilities",
@@ -44,18 +41,14 @@ public class FeedbackFileEditor : ModuleRules
                 "InputCore",
                 "HapticsManager",
                 "EditorStyle"
-                //"Sockets",
-				// ... add private dependencies that you statically link with here ...	
-			}
-			);
+            }
+            );
 
-        
         DynamicallyLoadedModuleNames.AddRange(
-			new string[]
-			{
-				// ... add any modules that your module loads dynamically here ...
-			}
-			);
+            new string[]
+            {
+            }
+            );
 
 	}
 }
