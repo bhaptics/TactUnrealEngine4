9a1529dbd736ae333d8cac566857c228f130e11c
Sangwon Choi <sangwon.choi@bhaptics.com>
2021-06-08 11:01:19 +0900

    Update minor

--- ue4_samples/SampleProject4_22/Plugins/HapticsManager/Source/FeedbackFileEditor/FeedbackFileEditor.Build.cs
+++ ue4_samples/SampleProject4_22/Plugins/HapticsManager/Source/FeedbackFileEditor/FeedbackFileEditor.Build.cs
@@ -34,7 +34,6 @@ public class FeedbackFileEditor : ModuleRules
                 "Engine", 
                 "Slate", 
                 "SlateCore", 
-                "FeedbackFileEditor",
                 "Json",
                 "JsonUtilities",
                 "Projects",
