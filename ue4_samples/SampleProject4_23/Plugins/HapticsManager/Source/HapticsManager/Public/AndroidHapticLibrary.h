d66f9fc3ce70649d82e5f8cda0458ca865c9d2cb
Sangwon Choi <sangwon.choi@bhaptics.com>
2021-07-23 10:14:57 +0900

    Update UI and remove unused

--- ue4_samples/SampleProject4_23/Plugins/HapticsManager/Source/HapticsManager/Public/AndroidHapticLibrary.h
+++ ue4_samples/SampleProject4_23/Plugins/HapticsManager/Source/HapticsManager/Public/AndroidHapticLibrary.h
@@ -54,34 +54,6 @@ class UAndroidHapticLibrary : public UBlueprintFunctionLibrary
 	static void SubmitRegistered(
 		FString key, FString altKey, float intensity, float duration, float xOffsetAngle, float yOffset);
 
-	//Scan for bHaptics devices
-	UFUNCTION(BlueprintCallable, meta = (DisplayName = "StartHapticDeviceScan", Keywords = "Haptics"), Category = "Haptics")
-		static void AndroidThunkCpp_StartScanning();
-
-	//Stop scanning for devices
-	UFUNCTION(BlueprintCallable, meta = (DisplayName = "StopHapticDeviceScan", Keywords = "Haptics"), Category = "Haptics")
-		static void AndroidThunkCpp_StopScanning();
-
-	//Check if its currently scanning
-	UFUNCTION(BlueprintPure, meta = (DisplayName = "IsHapticDeviceScanning", Keywords = "Haptics"), Category = "Haptics")
-		static bool AndroidThunkCpp_IsScanning();
-
-	//Pair with Haptic Device
-	UFUNCTION(BlueprintCallable, meta = (DisplayName = "PairHapticDevice", Keywords = "Haptics"), Category = "Haptics")
-	static void AndroidThunkCpp_Pair(FString DeviceAddress);
-
-	//Pair with Haptic Device and set Position
-	UFUNCTION(BlueprintCallable, meta = (DisplayName = "PairHapticDevicePosition", Keywords = "Haptics"), Category = "Haptics")
-	static void AndroidThunkCpp_PairFromPosition(FString DeviceAddress, FString DevicePosition);
-
-	//Unpair Haptic Device
-	UFUNCTION(BlueprintCallable, meta = (DisplayName = "UnpairHapticDevice", Keywords = "Haptics"), Category = "Haptics")
-	static void AndroidThunkCpp_Unpair(FString DeviceAddress);
-	
-	//Unpair all Haptic Devices
-	UFUNCTION(BlueprintCallable, meta = (DisplayName = "UnpairAll", Keywords = "Haptics"), Category = "Haptics")
-	static void AndroidThunkCpp_UnpairAll();
-	
 	//Ping Haptic Device
 	UFUNCTION(BlueprintCallable, meta = (DisplayName = "PingHapticDevice", Keywords = "Haptics"), Category = "Haptics")
 	static void AndroidThunkCpp_Ping(FString DeviceAddress);
@@ -97,6 +69,10 @@ class UAndroidHapticLibrary : public UBlueprintFunctionLibrary
 	//Toggle the Position of Device
 	UFUNCTION(BlueprintCallable, meta = (DisplayName = "ToggleDevicePosition", Keywords = "Haptics"), Category = "Haptics")
 	static void AndroidThunkCpp_TogglePosition(FString DeviceAddress);
+
+	//Toggle the Position of Device
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "BhapticsRefreshPairing", Keywords = "Haptics"), Category = "Haptics")
+	static void AndroidThunkJava_RefreshPairing();
 	   
 private:
 	static TArray<FDevice> FoundDevices;
