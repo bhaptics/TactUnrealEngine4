8d4a255a0de03e2f29096931e168a21baf041737
Sangwon Choi <sangwon.choi@bhaptics.com>
2021-09-06 15:06:53 +0900

    Setup for 4.25

---
+++ ue4_samples/SampleProject4_25/Plugins/HapticsManager/Source/HapticsManager/Public/Feedback/GloveFeedbackFile.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "FeedbackFile.h"
+#include "GloveFeedbackFile.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HAPTICSMANAGER_API UGloveFeedbackFile : public UFeedbackFile
+{
+	GENERATED_BODY()
+
+	
+};
